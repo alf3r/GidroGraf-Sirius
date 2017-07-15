c612a49c9fb196f150456e0827fd07c0a5480750
Andrey Handurin <handurin@mail.ru>
2017-07-15 08:46:11 +0300

    Исправения тест БД

--- src/test_openDB.py
+++ src/test_openDB.py
@@ -12,8 +12,8 @@ if __name__ == "__main__":
     v = 1    #скорость гидролокатора
     path2hyscanbin = r'/media/alf/Storage/hyscan-builder-linux/bin'
     path2hyscanprj = r'/media/alf/Storage/Hyscan5_projects'
-    project_name   = 'sirius'
-    track_name     = 'Track05'
+    project_name   = 'echo2'
+    track_name     = 'Track08'
 
     # Созадем экземпляр класса gidroGraf_DBreader
     DB = gg.Hyscan5wrapper(path2hyscanbin, path2hyscanprj, project_name)
@@ -46,18 +46,26 @@ if __name__ == "__main__":
         data_port      = cv2.resize(data_port,      dim, interpolation=cv2.INTER_AREA)
         data_starboard = cv2.resize(data_starboard, dim, interpolation=cv2.INTER_AREA)
 
-        cv2.imshow('PORT', data_port)
-        cv2.imshow('STARBOARD', data_starboard)
+
 
         if i == (count_reads - 1) * step:
             break
         else:
+            cv2.imshow('PORT', data_port)
+            cv2.imshow('STARBOARD', data_starboard)
             cv2.waitKey(10)
             i += 1
 
     # # Адаптивное выравнивание яркости и контраста
     # clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
     # processed = clahe.apply(converted)
+
+    ret, thresh = cv2.threshold(data_port, 127, 255, 0)
+    im2, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    cv2.drawContours(data_port, contours, -1, (0, 255, 0), 2)
+
+    cv2.imshow('PORT', data_port)
+    cv2.imshow('thresh', thresh)
 
     cv2.waitKey(0)
     cv2.destroyAllWindows()
